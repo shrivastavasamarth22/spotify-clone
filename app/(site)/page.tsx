2a8219350421233ea1ea5154f20b8e3370444030
Samarth Shrivastava <shrivatsavasamarth22@gmail.com>
2023-09-29 16:50:58 +0530

    Completed frontend layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header'
+import ListItem from '@/components/ListItem'
 
 export default function Home() {
   return (
@@ -11,8 +12,43 @@ export default function Home() {
       overflow-hidden
     ">
       <Header>
-        Header
+        <div className='mb-2'>
+          <h1 className='
+            text-whte
+            text-3xl
+            font-semibold
+          '>
+            Welcome Back
+          </h1>
+          <div
+            className="
+              grid
+              grid-cols-1
+              sm:grid-cols-2
+              xl:grid-cols-3
+              2xl:grid-cols-4
+              gap-3
+              mt-4
+            "
+          >
+              <ListItem 
+                image='/images/liked.png'
+                name='Liked Songs'
+                href='liked'
+              />
+          </div>
+        </div>
       </Header>
+      <div className='mt-2 mb-7 px-6'>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-white text-2xl font-semibold'>
+            Newest songs
+          </h1>
+        </div>
+        <div>
+          List of Songs!
+        </div>
+      </div>
     </div>
   )
 }
