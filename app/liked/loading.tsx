1e8ac8067366d9e297e56bbd960bc33c164f6516
Samarth Shrivastava <shrivatsavasamarth22@gmail.com>
2023-10-03 13:59:12 +0530

    Added error and loading screens to home, search and liked screens

---
+++ app/liked/loading.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { BounceLoader } from "react-spinners";
+import Box from "@/components/Box";
+
+const Loading = () => {
+    return (
+        <Box className="h-full flex items-center justify-center">
+            <BounceLoader 
+                color="#22C55E#"
+                size={40}
+            />
+        </Box>
+    )
+}
+
+export default Loading;
