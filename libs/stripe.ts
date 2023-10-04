2853216a5220b5665199f8158a588179b78a7bed
Samarth Shrivastava <shrivatsavasamarth22@gmail.com>
2023-10-04 17:27:39 +0530

    Formatted all the libs

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -1,12 +1,9 @@
-import Stripe from 'stripe';
+import Stripe from "stripe";
 
-export const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY ?? '',
-  {
-    apiVersion: '2023-08-16',
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
+    apiVersion: "2023-08-16",
     appInfo: {
-      name: 'Spotify Clone',
-      version: '0.1.0'
-    }
-  }
-);
+        name: "Spotify Clone",
+        version: "0.1.0",
+    },
+});
